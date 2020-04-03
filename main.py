aaadef46fb281028e16348c30a7ee292e257cfd8
Johannes Hoppe <info@johanneshoppe.com>
2020-04-03 16:09:36 +0200

    Remove explicit logger setup

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 """Lambda function that executes flake8, a static file linter."""
-import logging
-import sys
-
 from lintipy import CheckRun
 
-root_logger = logging.getLogger('')
-root_logger.setLevel(logging.DEBUG)
-root_logger.addHandler(logging.StreamHandler(sys.stdout))
 
 handle = CheckRun.as_handler(
     'flake8',
